6ef7f80ca7de31c070dd62125d0efb2abdd6171b
PrarththanaSumandiran <iamprarth11@gmail.com>
2022-06-27 15:13:21 +0530

    Get data in php

---
+++ insertstudentdata.php
@@ -0,0 +1,12 @@
+<?php
+$student_id =$_POST['student_id'];
+$firstname =$_POST['firstname'];
+$lastname =$_POST['lastname'];
+$dateofbirth =$_POST['dateofbirth'];
+$email =$_POST['email'];
+$department =$_POST['department'];
+echo "student_id"
+
+
+
+?>
