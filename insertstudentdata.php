75025f4e382995f64162c69319f119ca4c762412
PrarththanaSumandiran <iamprarth11@gmail.com>
2022-06-27 15:25:14 +0530

    Add notes

--- insertstudentdata.php
+++ insertstudentdata.php
@@ -1,10 +1,21 @@
 <?php
-$student_id =$_POST['student_id'];
-$firstname =$_POST['firstname'];
-$lastname =$_POST['lastname'];
-$dateofbirth =$_POST['dateofbirth'];
-$email =$_POST['email'];
-$department =$_POST['department'];
+$student_id = $_POST['student_id'];
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$dateofbirth = $_POST['dateofbirth'];
+$email = $_POST['email'];
+$department = $_POST['department'];
+/*
+if($email == "hackmail.com"){
+
+    echo "Please you can't be registerd";
+}else{
+
+    echo "You can be registerd";
+}
+
+*/
+
 echo "student_id"
 
 
